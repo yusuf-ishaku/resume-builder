d3aee7163fb34ec6b677d80b8f18c29fb897b399
yusuf <kingshakzy@gmail.com>
2023-03-06 16:58:35 -0800

    I call Alpha

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import './App.css'
 function App() {
   const[fname, setFname] = useState("First Name");
   const [lname, setLname] = useState("Last Name");
-  const[title, setTitle] = useState("Professional Title")
+  const[title, setTitle] = useState("Professional Title");
 
-
+  let creater = () =>{
+    let body = document
+  }
 
 
   return (
@@ -25,6 +27,9 @@ function App() {
             setTitle(e.target.value)
 
           }} placeholder='e.g. example@example.com' className="grow my-5 placeholder-gray-600 w-80 p-3 focus:outline-none rounded-md border-2 border-gray-800" required/>
+          <button className="w-fit border-gray-800 hover:text-red-800 hover:bg-white rounded-md text-white border-2 px-2 p-1 bg-red-800" onClick={creater}>
+            Add Education
+          </button>
         </div>
         <div className="w-1/2 p-4 h-[100vh] flex flex-col items-center border-2">
           <div className="min-w-[50%] max-w-[90%] min-h-32 p-4 text-center border-gray-400 border-2 max-h-40">
@@ -37,7 +42,9 @@ function App() {
               <span className="block uppercase w-fit p-1 px-3 text-lg h-fit text-gray-900 font-bold border-2 border-gray-300">
                 EDUCATION
               </span>
-              <p className="text-md text-gray-600 font-m">g</p>
+              <span className="text-md text-gray-900 font-m">
+                
+              </span>
             </div>
             <div className="row1 w-1/2 p-2">
               <span className="block uppercase w-fit p-1 px-3 text-lg h-fit text-gray-900 border-2 font-bold border-gray-300">
