36472e88ae9a43286bc32699942e9103d56b2d35
yusuf <kingshakzy@gmail.com>
2023-03-16 17:18:48 -0700

    Pages viewed done

--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -4,6 +4,9 @@ import { AppContext } from "../App";
 import { ColorLine } from "../components/color-line";
 import TCVS from "../assets/cv.png";
 import CT from "../assets/custom.png";
+import D1 from "../assets/d1.png";
+import D2 from "../assets/d2.png";
+import D3 from "../assets/d3.png";
 export const About = () =>{
     const {fly, setFly} = useContext(AppContext);
     useEffect(() =>{
@@ -32,6 +35,53 @@ export const About = () =>{
                     <figure className="w-1/2">
                         <img src={TCVS} alt="" />
                     </figure>
+                </section>
+                <section className="w-full h-[90vh] px-32">
+                    <div className="w-[60vw] -z-10 absolute -left-[25vw] h-[100vh] bg-blue-100 rounded-full"></div>
+                    <div className="relative h-[90vh] flex flex-row items-center z-full top-0 ">
+                        <section className="flex flex-col  items-center p-4 w-1/2">
+                            <div className="flex flex-row w-fit bg-white drop-shadow-xl p-3 rounded-md">
+                                <img className="object-contain" src={D1} alt="" />
+                                <div className="ml-5">
+                                    <header className="text-blue-900 text-xl font-black">Easy Online Resume Builder</header>
+                                    <article>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium modi assumenda.
+                                    </article>
+                                </div>
+                            </div>
+                            <div className="flex flex-row w-fit my-8 bg-white drop-shadow-xl p-3 rounded-md">
+                                <img className="object-contain" src={D2} alt="" />
+                                <div className="ml-5">
+                                    <header className="text-blue-900 text-xl font-black">Step by Step Expert Tips</header>
+                                    <article>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium modi assumenda.
+                                    </article>
+                                </div>
+                            </div>
+                            <div className="flex flex-row w-fit bg-white drop-shadow-xl p-3 rounded-md">
+                                <img className="object-contain" src={D3} alt="" />
+                                <div className="ml-5">
+                                    <header className="text-blue-900 text-xl font-black">Easy Online Resume Builder</header>
+                                    <article>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium modi assumenda.
+                                    </article>
+                                </div>
+                            </div>
+                        </section>
+                        <section className="w-1/2 px-4">
+                            <header className="text-4xl font-extrabold text-blue-900">
+                                <ColorLine></ColorLine>
+                                Why choose our platform?
+                            </header>
+                            <article className="my-4 text-lg text-gray-700 leading-8">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at dictum risus, non suscipit arcu. Quisque aliquam posuere tortor, sit amet convallis nunc scelerisque in.
+                            </article>
+                            <article className="my-4 text-lg text-gray-700 leading-8">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at dictum risus, non suscipit arcu. Quisque aliquam posuere tortor, sit amet convallis nunc scelerisque in.
+                            </article>
+                            <button className="bg-blue-400 px-4 stylus py-2 rounded-md text-white text-lg">Let's build your CV</button>
+                        </section>
+                    </div>
                 </section>
                 <section className="mx-32 flex pr-10 flex-row items-center justify-between rounded-xl overflow-hidden drop-shadow-2xl bg-white w-auto h-fit my-16">
                     <div className="">
