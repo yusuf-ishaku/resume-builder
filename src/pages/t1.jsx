ead67c08cb1d6e321f9d62c8f419377da0b6cff2
yusuf <kingshakzy@gmail.com>
2023-03-24 17:22:31 -0700

    Flies

--- src/pages/t1.jsx
+++ src/pages/t1.jsx
@@ -1,7 +1,8 @@
 import "../App.css";
 import { useContext, useEffect } from "react";
 import { AppContext } from "../App";
-import Ajoy from "../assets/ajoy.jpg"
+import Ajoy from "../assets/ajoy.jpg"; 
+import {MdLocationOn } from "react-icons/md"
 export const T1 = () =>{
    const {fly, setFly} = useContext(AppContext);
    useEffect(()=>{
@@ -10,17 +11,28 @@ export const T1 = () =>{
     return(
         <>
             <section className="w-full flex flex-row relative">
-                <section className="w-[40%] bg-gray-500 h-80 p-8">
+                <section className="w-[45%] bg-gray-500 h-fit p-6">
                     <section className="bg-white w-auto h-fit">
                         <section className="w-[45%] bg-blue-400 h-fit">
                             <img src={Ajoy} alt=""></img>
-                            <span className="block py-2 px-1 pl-2">
-                                <h1 className="text-white text-xl uppercase">{"Stephen Colbert"}</h1>
+                            <span className="block pt-2 px-1 pl-2">
+                                <h1 className="text-white font-semibold text-xl uppercase">{"Stephen Colbert"}</h1>
                             </span>
+                            <span className="block pt-1 px-1 pl-2">
+                                <h2 className="text-gray-700 font-semibold text-md uppercase">{"Web Developer"}</h2>
+                            </span>
+                            <div className="flex flex-col gap-4 p-2">
+                                <div className="flex flex-row  items-center text-blue-600">
+                                    <span className="w-10 mr-3 h-10 rounded-full flex flex-row items-center justify-center bg-white">
+                                        <MdLocationOn></MdLocationOn>
+                                    </span>
+                                    <h1 className="text-white">21 Street, Texas USA</h1>
+                                </div>
+                            </div>
                         </section>
                     </section>
                 </section>
-                <section className="w-[60%] bg-red-900 h-80">
+                <section className="w-[55%] bg-red-900 h-auto">
 
                 </section>
             </section>
