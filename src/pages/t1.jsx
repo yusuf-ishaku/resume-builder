ffd6f6aa39da7b1e9103b09023ba770d18542030
yusuf <kingshakzy@gmail.com>
2023-03-26 12:32:53 -0700

    Working on timeline piece

--- src/pages/t1.jsx
+++ src/pages/t1.jsx
@@ -180,10 +180,32 @@ export const T1 = () =>{
                                 </header>
                                 <section className="flex flex-col">
                                     <section className="flex flex-row">
-                                        <div className="mt-1 mr-2">
+                                        <div className="flex flex-col items-center justify-center mt-1 mr-2">
                                             <div className="w-3 h-3 flex flex-row items-center justify-center rounded-full bg-blue-400">
                                                 <div className="w-2 h-2 rounded-full bg-white"></div>
                                             </div>
+                                            <div className="w-[0.2rem] h-full relative bg-blue-400"></div>
+                                        </div>
+                                        <div>
+                                            <header className="text-gray-800 text-sm font-semibold mb-3">
+                                                <h1>2019-2020</h1>
+                                            </header>
+                                            <article>
+                                                <header>
+                                                    <h1 className="text-gray-800 text-sm font-semibold">Lorem ipsum dolor sit amet.</h1>
+                                                </header>
+                                                <p className="text-gray-600 text-xs font-medium">
+                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                                                </p>
+                                            </article>
+                                        </div>
+                                    </section>
+                                    <section className="flex flex-row ">
+                                        <div className="flex flex-col items-center justify-center mt-1 mr-2">
+                                            <div className="w-3 h-3 flex flex-row items-center justify-center rounded-full bg-blue-400">
+                                                <div className="w-2 h-2 rounded-full bg-white"></div>
+                                            </div>
+                                            <div className="w-[0.2rem] h-full bg-blue-400"></div>
                                         </div>
                                         <div>
                                             <header className="text-gray-800 text-sm font-semibold mb-3">
